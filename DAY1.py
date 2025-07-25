dee3b590badaa50de872468bee298bb095cf6957
Raghavisdev <raghavacenger007@gmail.com>
2025-07-25 21:51:00 +0530

    Day 1: Python Basics - Strings, Input, Booleans, and Operators

---
+++ DAY1.py
@@ -0,0 +1,9 @@
+# day1.py
+'''CALCULATING THE REMAINDER OF DIVISION'''
+
+dividend = int(input("Enter the number to be divided: "))
+divisor = int(input("Enter the number to divide by: "))
+
+remainder = dividend % divisor
+
+print(f"The remainder when {dividend} is divided by {divisor} is {remainder}.")
